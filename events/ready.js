0d5bf3409a0345562e25992a99f3ffa7a5a23281
IKAP <kaplas.iiro@gmail.com>
2022-01-21 13:16:40 +0200

    Add name and activity change to bot description

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,8 @@ module.exports = {
 	name: "ready",
 	once: true,
 	execute(client) {
+		client.user.setUsername("Bender");
+		client.user.setActivity("Bending the bendee");
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 	},
 };
